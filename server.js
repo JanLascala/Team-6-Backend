b3362ce26c1a1c909b92b2cbca93ac6c3aea07e9
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-05-05 17:26:27 +0200

    add 404 and internal error middlewares

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const vinyl_routers = require('./routes/vinyl_routers')
+const errorsHandler = require('./middlewares/errors.js')
+const notFound = require('./middlewares/error404.js')
 
 app.listen(port, () => {
     console.log(`server is running on http://localhost:${port} `)
@@ -11,7 +13,11 @@ app.get("/", (req, res) => {
     res.send("welcome to my api")
 })
 
-app.use('/vinyls', vinyl_routers)
-
 app.use(express.json());
 app.use(express.static('public'))
+app.use('/vinyls', vinyl_routers)
+
+//middlewares
+
+app.use(errorsHandler)
+app.use(notFound)
