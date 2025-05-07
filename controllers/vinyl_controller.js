984a172f87d672af3008127a903b4bcd9fa30a4f
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-05-07 16:39:41 +0200

    add show route with vinyl slugs

--- controllers/vinyl_controller.js
+++ controllers/vinyl_controller.js
@@ -20,7 +20,55 @@ ORDER BY releaseDate DESC;
     })
 }
 function show(req, res) {
-    res.send("this is the show route!")
+    const sql = `
+                SELECT
+                vinyls.id AS productId,
+                vinyls.slug,
+                vinyls.title,
+                vinyls.imgUrl AS vinylImg,
+                genres.genreName,
+                formats.formatName,
+                DATE_FORMAT(vinyls.releaseDate, '%d-%m-%Y') AS releaseDate,
+                vinyls.price,
+                vinyls.nAvailable,
+                authors.name AS authorName,
+                authors.imgUrl AS authorImg,
+                authors.description AS authorDescription,
+                publishers.name AS publisherName,
+                publishers.description AS publisherDescription,
+                COUNT(tracks.id) AS tracksNumber,
+                JSON_ARRAYAGG(
+                    JSON_OBJECT(
+                        'name', tracks.name,
+                        'length', tracks.length
+                    )
+                ) AS tracks
+                FROM vinyls
+                LEFT JOIN tracks ON tracks.vinylId = vinyls.id
+                LEFT JOIN authors ON authors.id = authorId
+                LEFT JOIN publishers ON publishers.id = publisherId
+                LEFT JOIN genres ON genres.id = genreId
+                LEFT JOIN formats ON formats.id = formatId
+                WHERE vinyls.slug = ?
+                GROUP BY
+                    vinyls.id, vinyls.slug, vinyls.title, vinyls.imgUrl, genres.genreName,
+                    formats.formatName, vinyls.releaseDate, vinyls.price, vinyls.nAvailable,
+                    authors.name, authors.imgUrl, publishers.name
+                ORDER BY releaseDate DESC;
+
+                `
+    const slug = req.params.slug;
+
+    connection.query(sql, [slug], (err, results) => {
+        if (err) return res.status(500).json({ error: err })
+        //console.log(results)
+        if (results.length === 0) {
+            return res.status(404).json({ message: 'Vinyl not found' });
+        }
+
+        res.json(results[0])
+        console.log("show route used!")
+    })
 }
 //recent route
 function recent(req, res) {
