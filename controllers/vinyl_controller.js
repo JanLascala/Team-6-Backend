dc1f454bb8654ee90df8a8570e2da782614a0751
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-05-07 10:18:55 +0200

    changed index property names to all be camelCase

--- controllers/vinyl_controller.js
+++ controllers/vinyl_controller.js
@@ -3,7 +3,7 @@ const connection = require('../data/data.js')
 
 function index(req, res) {
     const sql = `
-                SELECT vinyls.id as productId, vinyls.slug, vinyls.title, vinyls.img_url as vinylImg, vinyls.genre, vinyls.format, vinyls.release_date, vinyls.price, vinyls.n_available, authors.name as authorName, authors.img_url as authorImg, publishers.name as publisherName
+                SELECT vinyls.id as productId, vinyls.slug, vinyls.title, vinyls.img_url as vinylImg, vinyls.genre, vinyls.format, vinyls.releaseDate, vinyls.price, vinyls.nAvailable, authors.name as authorName, authors.img_url as authorImg, publishers.name as publisherName
 FROM vinyls
 LEFT JOIN authors on authors.id = author_id
 LEFT JOIN publishers on publishers.id = publisher_id
