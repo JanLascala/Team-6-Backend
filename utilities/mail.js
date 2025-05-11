5829621303947ebade3543e07c67cccc509aa722
mattiapennisi <mattiapennisi0@gmail.com>
2025-05-11 03:27:23 +0200

    add mail.js for nodemailer integration and testing setup

---
+++ utilities/mail.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+
+nodemailer.createTestAccount((err, account) => {
+    if (err) {
+        console.error("Failed to create a testing account. " + err.message);
+        return;
+    }
+
+    const transporter = nodemailer.createTransport({
+        host: process.env.MAILTRAP_HOST,
+        port: process.env.MAILTRAP_PORT,
+        secure: false,
+        auth: {
+            user: account.user,
+            pass: account.pass
+        }
+    });
+
+    transporter
+        .sendMail({
+            from: "Example app <no-reply@example.com>",
+            to: "user@example.com",
+            subject: "Hello from tests ✔",
+            text: "This message was sent from a Node.js integration test.",
+        })
+        .then((info) => {
+            console.log("Message sent: %s", info.messageId);
+            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        })
+        .catch(console.error);
+});
